6ce0618528f7a742180ec3de6e3249f221116428
MichaelZolotov77 <michaelzol@yandex.ru>
2024-01-07 17:25:32 +0300

    Практика на фильтрацию

--- app.js
+++ app.js
@@ -42,3 +42,84 @@ console.log(marriedUsers);
 // проверим, содержит ли movies значение "Snatch"
 const snatchUsers = users.filter((user) => user.movies.includes("Snatch"));
 console.log(snatchUsers);
+
+const names = [
+  "Mikhail",
+  "Ivan",
+  "Albert",
+  "John",
+  "Ian",
+  "Petr",
+  "Alexandr",
+  "Oleg",
+  "Jaroslav",
+  "Vsevolod",
+];
+
+console.log(names[0].length);
+const shortNames = names.filter((person) => person.length <= 5);
+console.log(shortNames);
+
+const products = [
+  {
+    id: 1,
+    name: "Intro to JavaScript",
+    category: "books",
+    price: 150,
+  },
+  {
+    id: 2,
+    name: "Intro to CSS",
+    category: "books",
+    price: 120,
+  },
+  {
+    id: 3,
+    name: "Deep dive into JavaScript",
+    category: "books",
+    price: 350,
+  },
+  {
+    id: 4,
+    name: "Intro to PHP",
+    category: "books",
+    price: 250,
+  },
+  {
+    id: 5,
+    name: "Deep dive into PHP",
+    category: "books",
+    price: 350,
+  },
+];
+
+const cheapProducts = products.filter((product) => product.price < 300);
+console.log(cheapProducts);
+
+const developers = [
+  {
+    id: 1,
+    fullName: "Ivan Ivanov",
+    skills: ["HTML", "CSS", "Git", "Gulp", "Pug"],
+  },
+  {
+    id: 2,
+    fullName: "Petr Petrov",
+    skills: ["HTML", "CSS", "Git", "JavaScript", "npm"],
+  },
+  {
+    id: 3,
+    fullName: "Ian Melnikov",
+    skills: ["HTML", "CSS", "Git", "JavaScript", "TypeScript"],
+  },
+  {
+    id: 4,
+    fullName: "Antonio Banderas",
+    skills: ["HTML", "CSS", "Git", "JavaScript", "TypeScript", "React"],
+  },
+];
+
+const tsDevelopers = developers.filter((dev) =>
+  dev.skills.includes("TypeScript")
+);
+console.log(tsDevelopers);
