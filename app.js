74582ea3ce68e21d62756b4913b30eb5b7c7a319
MichaelZolotov77 <michaelzol@yandex.ru>
2024-01-07 17:01:35 +0300

    Метод filter()

--- app.js
+++ app.js
@@ -1,29 +1,44 @@
-const developers = [
+// Filter
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// функция-колбек возвращает true или false, если вернет true,
+// то значение запишется в новый массив
+const oddNumbers = numbers.filter((num) => num % 2 !== 0);
+console.log(oddNumbers);
+console.log(numbers);
+
+// Такие массивы из объектов называют коллекциями
+const users = [
   {
     id: 1,
-    fullName: "Anton Petrov",
-    skills: ["HTML", "CSS", "JavaScript", "Git", "React"],
-    salary: 1000,
+    name: "Brad Pette",
+    isMarried: true,
+    movies: ["Snatch", "Inglorious Basterdes"],
   },
   {
     id: 2,
-    fullName: "Ivan Ivanov",
-    skills: ["HTML", "CSS", "JavaScript", "Git", "Vue"],
-    salary: 950,
+    name: "Diana Moore",
+    isMarried: false,
+    movies: ["Inglorious Basterdes"],
   },
   {
     id: 3,
-    fullName: "Albert Sidorov",
-    skills: ["HTML", "CSS", "JavaScript", "Git", "jQuery"],
-    salary: 850,
+    name: "Olivie Giroud",
+    isMarried: true,
+    movies: ["Inglorious Basterdes"],
+  },
+  {
+    id: 4,
+    name: "Scott Parker",
+    isMarried: false,
+    movies: ["Inglorious Basterdes"],
   },
 ];
 
-const middleDevelopers = developers.map((person) => {
-  return {
-    ...person,
-    skills: [...person.skills, "TypeScript"],
-    salary: person.salary + 500,
-  };
-});
-console.log(middleDevelopers);
+// если одно из полей будет соответствовать условию, то запишется весь объект
+// если нет, то весь объект не запишется в новый массив
+const marriedUsers = users.filter((user) => user.isMarried);
+console.log(marriedUsers);
+// проверим, содержит ли movies значение "Snatch"
+const snatchUsers = users.filter((user) => user.movies.includes("Snatch"));
+console.log(snatchUsers);
