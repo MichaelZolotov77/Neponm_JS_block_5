b01e286d3fe81600e6428fe97c9f391ba9596703
MichaelZolotov77 <michaelzol@yandex.ru>
2024-01-07 21:24:08 +0300

    Методы find() и findIndex()

--- app.js
+++ app.js
@@ -1,76 +1,41 @@
-// Reduce
-const staff = [
+// Find in collection
+const cities = ["London", "Paris", "Madrid", "Lisboa"];
+console.log(cities.includes("Madrid")); // true
+console.log(cities.indexOf("Paris")); // 1
+
+const developers = [
   {
     id: 1,
-    name: "John Doe",
-    salary: 1000,
+    fullname: "John Doe",
+    skills: ["HTML", "CSS", "JavaScript"],
+    area: "frontend",
   },
   {
     id: 2,
-    name: "Sara Smith",
-    salary: 900,
+    fullname: "Adam Smith",
+    skills: ["HTML", "CSS", "JavaScript", "React"],
+    area: "frontend",
   },
   {
     id: 3,
-    name: "Elton John",
-    salary: 1100,
+    fullname: "Will Dowson",
+    skills: ["HTML", "PHP", "Laravel"],
+    area: "backend",
   },
   {
     id: 4,
-    name: "Mo Williams",
-    salary: 1000,
+    fullname: "Sam Wilson",
+    skills: ["HTML", "CSS", "JavaScript", "PHP"],
+    area: "fullstack",
   },
 ];
 
-const budget = staff.reduce((acc, user) => {
-  return acc + user.salary;
-}, 0);
+// Возвратит элемент полностью
+const result = developers.find((item) => item.area === "frontend");
+console.log(result);
 
-console.log(budget);
+const result2 = developers.find((item) => item.skills.includes("React"));
+console.log(result2);
 
-const budgetForSmallPersonal = staff.reduce((acc, user) => {
-  if (user.salary < 1000) {
-    return acc + user.salary;
-  }
-
-  return acc;
-}, 0);
-console.log(budgetForSmallPersonal);
-
-const salaries = staff.reduce((acc, user) => {
-  return [...acc, user.salary];
-}, []);
-console.log(salaries);
-
-const order = [
-  {
-    id: 1,
-    name: "Лопата",
-    price: 1000,
-    quantity: 1,
-  },
-  {
-    id: 2,
-    name: "Удочка",
-    price: 1200,
-    quantity: 2,
-  },
-  {
-    id: 3,
-    name: "Ведро",
-    price: 500,
-    quantity: 3,
-  },
-  {
-    id: 4,
-    name: "Мороженое",
-    price: 100,
-    quantity: 8,
-  },
-];
-
-const totalPrice = order.reduce(
-  (acc, ord) => acc + ord.price * ord.quantity,
-  0
-);
-console.log(totalPrice);
+const result3 = developers.findIndex((item) => item.skills.includes("React"));
+console.log(result3);
