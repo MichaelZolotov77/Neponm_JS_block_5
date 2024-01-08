11fee534bd7a9ee23b629da09cca79b40da646de
MichaelZolotov77 <michaelzol@yandex.ru>
2024-01-08 19:15:00 +0300

    Практика по деструктуризации объекта

--- app.js
+++ app.js
@@ -27,3 +27,26 @@ console.log(area, id);
 
 // Деструктуризация в функциях
 function fn([a, b, c], { x, y }) {}
+
+const link = {
+  href: "#",
+  title: "simple link",
+  target: "blank",
+  className: "link",
+  id: null,
+  children: {
+    span: {
+      content: "Click me",
+      className: "anchor",
+    },
+  },
+};
+
+const {
+  target,
+  className,
+  children: {
+    span: { content, className: renamed },
+  },
+} = link;
+console.log(target, className, content, renamed);
