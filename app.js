00a702c5a7d6ad2ceb7a9912dfb5ef441fd35528
MichaelZolotov77 <michaelzol@yandex.ru>
2024-01-09 17:41:23 +0300

    JSON

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-// Стандартные встроенные объекты
+// JSON - JavaScript Object Notation
 
-console.log(Number("12"));
-console.log(Number(true)); // 1
-console.log(Number(false)); // 0
-console.log(String(12));
-console.log(Boolean("")); // false
-console.log(Boolean([])); // true
+const obj = { a: 1, b: 2 };
+console.log(String(obj)); // [object Object]
 
-const obj = { a: 1, b: 2, c: 3 };
+// Преобразование объекта в JSON. JSON это строка.
+console.log(JSON.stringify(obj)); // {"a":1,"b":2}
 
-console.log(Object.keys(obj)); // ['a', 'b', 'c']
-console.log(Object.values(obj)); // [1, 2, 3]
-console.log(Object.entries(obj)); // [Array(2), Array(2), Array(2)]
+const a = `{
+    "firstName": "Mikhail",
+    "age": 36,
+    "skills": ["CSS", "JavaScript"],
+    "lahguages": {
+      "key": "value"
+    }
+  }`;
 
-console.log(Array.isArray(obj)); // false
-console.log(Array.from("hello")); // ['h', 'e', 'l', 'l', 'o']
-console.log(Array.of("hello", 1, true)); // ['hello', 1, true]
+// Преобразование строки JSON в объект
+const myObj = JSON.parse(a);
+console.log(myObj.firstName);
+console.log(myObj);
