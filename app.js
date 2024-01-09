b0e5aaad4b6e8b60eccf4a1e2f790144d14cb003
MichaelZolotov77 <michaelzol@yandex.ru>
2024-01-09 22:29:17 +0300

    Проект "Витрина"

--- app.js
+++ app.js
@@ -1,51 +1,62 @@
-// JSON - JavaScript Object Notation
-
-const obj = { a: 1, b: 2 };
-console.log(String(obj)); // [object Object]
-
-// Преобразование объекта в JSON. JSON это строка.
-console.log(JSON.stringify(obj)); // {"a":1,"b":2}
-
-const a = `{
-    "firstName": "Mikhail",
-    "age": 36,
-    "skills": ["CSS", "JavaScript"],
-    "lahguages": {
-      "key": "value"
-    }
-  }`;
-
-// Преобразование строки JSON в объект
-const myObj = JSON.parse(a);
-console.log(myObj.firstName);
-console.log(myObj);
-
-const players = [
+const products = [
   {
     id: 1,
-    name: "Andres",
-    surname: "Iniesta",
-    club: "Vissel Cobe",
+    title: "Lenovo Yoga",
+    price: 3000,
   },
   {
     id: 2,
-    name: "Eden",
-    surname: "Hazard",
-    club: "Real Madrid",
+    title: "Acer Aspire",
+    price: 1800,
   },
   {
     id: 3,
-    name: "Mo",
-    surname: "Salah",
-    club: "Liverpool",
-  },
-  {
-    id: 4,
-    name: "Lionel",
-    surname: "Messi",
-    club: "Barcelona",
+    title: "Dell Vostro",
+    price: 3400,
   },
 ];
 
-const jsonPlayers = JSON.stringify(players);
-console.log(jsonPlayers);
+let order = [];
+
+function addToBasket(productId) {
+  // TODO: добавить проверку наличия товара в заказе (при наличии выдать alert, что товар уже в корзине)
+  if (order.find((el) => el.id === productId))
+    return alert("Товар уже в корзине");
+
+  // TODO: если товар еще не в корзине, добавить его из массива products
+  const product = products.find((item) => item.id === productId);
+  order = [...order, product];
+  // Эти строчки не трогаем, они отвечают за переотрисовку страницы
+  renderCart();
+  rerenderTotalPrice();
+}
+
+function removeFromBasket(productId) {
+  // TODO: описать логику удаления товара из корзины
+  order = order.filter((item) => item.id !== productId);
+  // Эти строчки не трогаем, они отвечают за переотрисовку страницы
+  renderCart();
+  rerenderTotalPrice();
+}
+
+function rerenderTotalPrice() {
+  // TODO: опишите функционал подсчета общей стоимости заказа
+  const totalPrice = order.reduce((acc, item) => {
+    return acc + item.price;
+  }, 0);
+  // Не меняйте эту строчку
+  document.getElementById("total").innerText = totalPrice;
+}
+
+// Этот метод остается без изменений
+function renderCart() {
+  const cart = document.getElementById("basket-items");
+
+  cart.innerHTML = "";
+  order.forEach((item) => {
+    const el = document.createElement("li");
+    el.innerText = item.title;
+    el.onclick = () => removeFromBasket(item.id);
+    cart.appendChild(el);
+  });
+}
