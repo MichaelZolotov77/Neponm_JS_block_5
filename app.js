a62efeaab63c36d03037bdc46957704076f138de
MichaelZolotov77 <michaelzol@yandex.ru>
2024-01-09 17:47:46 +0300

    Преобразование данных в формат JSON практика

--- app.js
+++ app.js
@@ -19,3 +19,33 @@ const a = `{
 const myObj = JSON.parse(a);
 console.log(myObj.firstName);
 console.log(myObj);
+
+const players = [
+  {
+    id: 1,
+    name: "Andres",
+    surname: "Iniesta",
+    club: "Vissel Cobe",
+  },
+  {
+    id: 2,
+    name: "Eden",
+    surname: "Hazard",
+    club: "Real Madrid",
+  },
+  {
+    id: 3,
+    name: "Mo",
+    surname: "Salah",
+    club: "Liverpool",
+  },
+  {
+    id: 4,
+    name: "Lionel",
+    surname: "Messi",
+    club: "Barcelona",
+  },
+];
+
+const jsonPlayers = JSON.stringify(players);
+console.log(jsonPlayers);
